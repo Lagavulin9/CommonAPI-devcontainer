3910ce5745a41d67e10fc4bf7100655097afc5af
jinholee <jinholee@student.42seoul.kr>
2024-03-15 07:31:48 +0900

    initial commit

---
+++ .devcontainer/scripts/container-set.sh
@@ -0,0 +1,13 @@
+# Create remote user
+useradd vscode -m
+echo 'vscode:vscode' | chpasswd
+usermod -aG sudo vscode
+chown vscode:vscode /workspace
+
+# Create a symbolic link for .ini
+if [ -f "conf/commonapi.ini" ]; then
+	ln -s "$(pwd)/conf/commonapi.ini" /etc/commonapi.ini
+    echo "Symbolic link for commonapi.ini created successfully."
+else
+    echo "commonapi.ini does not exist."
+fi
