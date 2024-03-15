8b3838cd316248cac7d6e6cb6dff6a8e132fffbe
jinholee <jinholee@student.42seoul.kr>
2024-03-15 09:32:24 +0900

    base settings

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,1 +1,9 @@
 # Routines for postStart
+
+# Create a symbolic link for .ini
+if [ -f "conf/commonapi.ini" ]; then
+    export COMMONAPI_CONFIG=$(pwd)/conf/commonapi.ini
+    echo "Using user configuration /conf/commonapi.ini."
+else
+    echo "Using image default /etc/commonapi.ini."
+fi
